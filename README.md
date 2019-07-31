206b0305db4b4df182d5ce0166262dbc0e2b4546
Koi Nil <khoinguyen998@gmail.com>
2019-07-31 13:45:33 -0700

    Added excursion and predelay

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-Dattorro's Reverb Implemented in WebAudio AudioWorklet
+Dattorro's Reverb Implemented in WebAudio AudioWorklet, in Javascript
 =====
 
 To see the demo, refer to project page above.
 
 ## Characteristics
 
-* All paramters are k-rated.
+* All parameters are k-rated.
 * Accepts one input with two channels and 
 * Returns one output with two channels.
 
 ## Usage
 
-You can use this in your project like below. 
+You can use this in your project like so: 
 
 ```javascript
 yourAudioContext.addModule('dattorroReverb.js').then(() => {
@@ -21,6 +21,57 @@ yourAudioContext.addModule('dattorroReverb.js').then(() => {
 	reverb.connect(destinationNode);
 });
 ```
+
+## Personal Considerations
+
+Here I address the considerations I made that was not explicitly specified in the Dattorro paper.
+
+### Interpolation of Delay Lines
+
+**Quote:** "Linear interpolation, or better yet, all-pass interpolation can be efficiently employed [to ...] the two indicated delay lines [...]" (1.3.7, p. 665)
+
+**Decision:** No interpolation was employed.
+
+**Rationale:** Although all-pass interpolation is inexpensive - one multiplication, two additions ([j.o.s.](https://ccrma.stanford.edu/~jos/pasp/First_Order_Allpass_Interpolation.html)), I've decided not to implementation those. This decision might be reverted in the future.
+
+
+### 32 Sample Max Excursion
+
+**Quote:** "at a peak excursion of about 8 samples for a sample rate of about 29.8 kHz." (footnote 14, p. 665) and "EXCURSION = 16 (Maximum peak sample excursion of delay modulation)" (Table 1, p. 663)
+
+**Decision:** 32 samples for maximum excursion was chosen.
+
+**Rationale:** At first, the two quotations appear contradictory. However, I believe Dattorro meant 16 samples for the _range_ of excursion and 8 samples for the peak).
+
+### Unipolar Excursion
+
+**Quote:** [no quotation available]
+
+**Decision:** Delay length including excursion is: `length + (peak_excursion + excursion(t))` instead of `length + excursion(t)`
+
+**Rationale:** Because of the way the delay lines are implemented, excursions are not oscillating around original delay length, but the delays are contracting by twice the length from center to peak of the original excursion.
+
+### Parameterized Decay Diffusion 2
+
+**Quote:** "decay diffusion 2 = decay + 0.15, floor = 0.25, ceiling = 0.5" (Table 1, p. 663)
+
+**Decision:** Clipping the parameter were not implemented, nor was it bounded to being `decay + 0.15`
+
+**Rationale:** Elsewhere on top of page 664, the phrase "labeled by the knobs "decay diffusion 1" and "decay diffusion 2"" appears to me that they are meant to be controllable knobs and the quotation in Table 1 is merely a suggestion of what the value should be according to the `decay`. From the user experience standpoint, the two decay diffusions need not be exposed to the users. However, I am sticking to the paper on this issue.
+
+### PreDelay length
+
+**Quote:** Figure 1 of the paper
+
+**Decision:** Max Predelay length is the sampling rate minus 1 (i.e., ~ 1 second).
+
+**Rationale:** No rationale here, although 1 second is ridiculously long.
+
+## Open Questions
+
+### Parameterizing the decayRate as T60.
+
+Currently, the `decayRate` parameter is a number which the signal in the tank will be (twice) scaled by. What is the [T60](https://ccrma.stanford.edu/~jos/mdft/Audio_Decay_Time_T60.html) of this reverb with respect to the `decayRate`?
 
 ## References
 
--- dattorroReverb.js
+++ dattorroReverb.js
@@ -2,6 +2,12 @@ class DattorroReverb extends AudioWorkletProcessor {
 	
 	static get parameterDescriptors() {
 		return [{
+				name: 'preDelay',
+				defaultValue: 0,
+				minValue: 0,
+				maxValue: sampleRate-1,
+				automationRate: "k-rate"
+		},{
 			 	name: 'bandwidth',
 				defaultValue: 0.9999,
 				minValue: 0,
@@ -45,9 +51,9 @@ class DattorroReverb extends AudioWorkletProcessor {
 				automationRate: "k-rate"
 		},{
 			 	name: 'excursion',
-				defaultValue: 0,
+				defaultValue: 16,
 				minValue: 0,
-				maxValue: 16,
+				maxValue: 32,
 				automationRate: "k-rate"
 		},{
 			 	name: 'wet',
@@ -67,9 +73,10 @@ class DattorroReverb extends AudioWorkletProcessor {
 	constructor(options) {
 		super(options); 
 
-		// this.excursion  = 16;
-
 		this._Delays    = [];
+		this._pDLength  = sampleRate + (128 - sampleRate%128)
+		this._preDelay  = new Float32Array(this._pDLength);
+		this._pDWrite    = 0;
 		this._lp1       = 0.0;
 		this._lp2       = 0.0;
 		this._lp3       = 0.0;
@@ -96,7 +103,6 @@ class DattorroReverb extends AudioWorkletProcessor {
 			0.008937872,0.099929438,0.064278754,0.067067639,0.066866033,0.006283391,0.035818689,
 			0.011861161,0.121870905,0.041262054,0.08981553 ,0.070931756,0.011256342,0.004065724
 		], x => Math.round(this.conv(x)));
-
 	}
 
 	conv (value) {
@@ -104,7 +110,7 @@ class DattorroReverb extends AudioWorkletProcessor {
 	}
 
 	makeDelay (length, noConversion) { 
-		// len, delay, write, read
+		// len, array, write, read
 		let len = Math.round(this.conv(length));
 		this._Delays.push([
 			len,
@@ -130,37 +136,36 @@ class DattorroReverb extends AudioWorkletProcessor {
 		return this._Delays[index][1][this._Delays[index][2]];
 	}
 
-	updateDelays () {
-		for (let i = 0; i < this._Delays.length; i++) {
-			this._Delays[i][2]++;
-			this._Delays[i][3]++;
-			this._Delays[i][2] %= this._Delays[i][0];
-			this._Delays[i][3] %= this._Delays[i][0];
-		}
-	}
-
 	// Only accepts one input, two channels.
 	// Spits one output, two channels.
-	process (inputs, outputs, paramters) {
-		let bw = paramters.bandwidth[0]           ,
-			fi = paramters.inputDiffusion1[0]     , 
-			si = paramters.inputDiffusion2[0]     ,
-			dc = paramters.decay[0]               ,
-			ft = paramters.decayDiffusion1[0]     ,
-			st = paramters.decayDiffusion2[0]     ,
-			dp = paramters.damping[0]             ,
-			we = paramters.wet[0]            * 0.6, // lo and ro are both multiplied by 0.6 anyways
-			dr = paramters.dry[0]                 ;
+	process (inputs, outputs, parameters) {
+		let pd = ~~parameters.preDelay[0]          ,
+			bw = parameters.bandwidth[0]           ,
+			fi = parameters.inputDiffusion1[0]     , 
+			si = parameters.inputDiffusion2[0]     ,
+			dc = parameters.decay[0]               ,
+			ft = parameters.decayDiffusion1[0]     ,
+			st = parameters.decayDiffusion2[0]     ,
+			dp = parameters.damping[0]             ,
+			ex = parameters.excursion[0]           ,
+			we = parameters.wet[0]            * 0.6, // lo and ro are both multiplied by 0.6 anyways
+			dr = parameters.dry[0]                 ;
 
 		let lOut	= outputs[0][0];
 		let rOut	= outputs[0][1];
+
+		// write to predelay
+		this._preDelay.set(
+			Float32Array.from(inputs[0][0], (n, i) => (n + inputs[0][1][i]) * 0.5),
+			this._pDWrite
+		);
 
 		let i = 0;
 		while (i < 128) {
 			let lo = 0.0;
 			let ro = 0.0;
 
-			this._lp1        = (inputs[0][0][i] + inputs[0][1][i]) * 0.5 * bw + (1 - bw) * this._lp1;
+			this._lp1        = this._preDelay[(this._pDLength + this._pDWrite - pd + i)%this._pDLength] * bw + (1 - bw) * this._lp1;
 
 			// Please note: The groupings and formatting below does not bare any useful information about 
 			//              the topology of the network. I just want orderly looking text.
@@ -173,19 +178,22 @@ class DattorroReverb extends AudioWorkletProcessor {
 
 			let split       =  si *  this.readPreDelay(3) + this.readDelay(3);
 
+			// 1Hz (footnote 14, pp. 665)
+			let excursion   =  ~~(ex * (1+ Math.cos(currentTime*6.28))); // Non-negative means I can do the ~~flooring trick
+			
 			// left
-			this.writeDelay( 4, split +       dc * this.readDelay(11)          + ft * this.readDelay(4)    ); // tank diffuse 1
-			this.writeDelay( 5,                    this.readDelay(4)           - ft * this.readPreDelay(4) ); // long delay 1
-			this._lp2        =          (1 - dp) * this.readDelay(5)           + dp * this._lp2             ; // damp 1
-			this.writeDelay( 6,               dc * this._lp2                   - st * this.readDelay(6)    ); // tank diffuse 2
-			this.writeDelay( 7,                    this.readDelay(6)           + st * this.readPreDelay(6) ); // long delay 2
+			this.writeDelay( 4, split +       dc * this.readDelay(11)             + ft * this.readDelayAt(4, excursion) ); // tank diffuse 1
+			this.writeDelay( 5,                    this.readDelayAt(4, excursion) - ft * this.readPreDelay(4)           ); // long delay 1
+			this._lp2        =          (1 - dp) * this.readDelay(5)              + dp * this._lp2                       ; // damp 1
+			this.writeDelay( 6,               dc * this._lp2                      - st * this.readDelay(6)              ); // tank diffuse 2
+			this.writeDelay( 7,                    this.readDelay(6)              + st * this.readPreDelay(6)           ); // long delay 2
 
 			// right
-			this.writeDelay( 8, split +       dc * this.readDelay(7)           + ft * this.readDelay(8)     ); // tank diffuse 3
-			this.writeDelay( 9,                    this.readDelay(8)           - ft * this.readPreDelay(8)  ); // long delay 3
-			this._lp3        =          (1 - dp) * this.readDelay(9)           + dp * this._lp3              ; // damper 2
-			this.writeDelay(10,               dc * this._lp3                   - st * this.readDelay(10)    ); // tank diffuse 4
-			this.writeDelay(11,                    this.readDelay(10)          + st * this.readPreDelay(10) ); // long delay 4
+			this.writeDelay( 8, split +       dc * this.readDelay(7)              + ft * this.readDelayAt(8, excursion) ); // tank diffuse 3
+			this.writeDelay( 9,                    this.readDelayAt(8, excursion) - ft * this.readPreDelay(8)           ); // long delay 3
+			this._lp3        =          (1 - dp) * this.readDelay(9)              + dp * this._lp3                       ; // damper 2
+			this.writeDelay(10,               dc * this._lp3                      - st * this.readDelay(10)             ); // tank diffuse 4
+			this.writeDelay(11,                    this.readDelay(10)             + st * this.readPreDelay(10)          ); // long delay 4
 
 			lo =  this.readDelayAt( 9, this._taps[0])
 				+ this.readDelayAt( 9, this._taps[1])
@@ -208,9 +216,17 @@ class DattorroReverb extends AudioWorkletProcessor {
 			rOut[i] = inputs[0][1][i] * dr + ro * we;
 
 			i++;
-
-			this.updateDelays();
+			// This below could be optimized so that we only update
+			// our indexes every 128-sample
+			for (let j = 0; j < this._Delays.length; j++) {
+				let d = this._Delays[j];
+				d[2] = (d[2] + 1) % d[0];
+				d[3] = (d[3] + 1) % d[0]; 
+			}
 		}
+
+		// Update preDelay index
+		this._pDWrite = (this._pDWrite + 128) % this._pDLength;
 
 		return true;
 	}
